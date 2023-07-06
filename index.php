26bcb53f3f40750f41b4280f32f12520aaee4d20
Numero Uno <numerounodeveloper@gmail.com>
2023-07-06 12:53:04 -0400

    fix

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ if (strpos($path, 'tutorials/') !== false) {
    <link href="/readernook/css/slidestyles.css" rel="stylesheet" />
    <link href="/readernook/css/smtheme-v1.05.css" rel="stylesheet" />
    <script src="/readernook/web/common-editor-function.js"></script>
-   <script src="/readernook/web/common-function-v1.3.js"></script>
+   <script src="/readernook/web/common-function-v1.4.js"></script>
    <!-----
          <script src="/readernook/web/common-function-mini.js"></script>
          -->
